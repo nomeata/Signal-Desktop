99520d1da5fbfd2f589411a15dfb17e0b8b35e79
Joachim Breitner <mail@joachim-breitner.de>
2016-12-29 10:04:17 +0100

    Move archive functionality into separate file

---
+++ js/export_archive.js
@@ -0,0 +1,81 @@
+(function () {
+    'use strict';
+    window.Whisper = window.Whisper || {};
+
+    function ExportArchive(options) {
+	this.conversation = options.conversation;
+	this.window = options.window;
+    }
+
+    ExportArchive.prototype = {
+	export: function(onSuccess) {
+          var archiver = this;
+
+          archiver.window.chrome.fileSystem.chooseEntry( {
+              type: 'saveFile',
+              suggestedName: 'signal.zip',
+                accepts: [ { description: 'Zip files (*.zip)',
+                             extensions: ['zip']} ],
+                acceptsAllTypes: true
+              }, function (fileEntry) {
+                var shadow_view = new Whisper.ConversationStaticView({
+                    model: archiver.conversation,
+                    window: archiver.window
+                });
+
+                var export_doc = $('<html>')
+                        .append($('<head>')
+                            .append($('<link href="stylesheets/manifest.css" rel="stylesheet" type="text/css"/>'))
+                            .append($('<meta charset="utf-8"/>'))
+                            .append($('<meta content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" name="viewport"/>'))
+                            .append($('<meta name="viewport" content="width=device-width, initial-scale=1"/>'))
+                            .append($('<title>Signal</title>')))
+                        .append($('<body>').toggleClass('android')
+                            .append(shadow_view.el));
+
+                var conversation_source = export_doc[0].outerHTML;
+
+                var mediablobs = archiver.conversation.messageCollection
+                    .chain()
+                    .map(function (message) {
+                        return message.get('attachments').map(function (attachment,idx) {
+                            var url = 'media/'+ message.id + '-' + idx;
+                            return {
+                                url: url,
+                                blob: new Blob([attachment.data], {type: attachment.contentType})
+                           };
+                        });
+                    })
+                    .flatten()
+                    .value();
+
+                  zip.workerScriptsPath = '/components/zip.js/WebContent/';
+                  zip.createWriter(new zip.FileWriter(fileEntry,"application/zip"), function(writer) {
+                    writer.add("index.html", new zip.TextReader(conversation_source), function() {
+                      chrome.runtime.getPackageDirectoryEntry(function (packageDir) {
+                          packageDir.getFile('stylesheets/manifest.css',{},function (fileEntry) {
+                              fileEntry.file(function (blob) {
+                                  writer.add("stylesheets/manifest.css", new zip.BlobReader(blob), function() {
+                                    writer.add(mediablobs[0].url, new zip.BlobReader(mediablobs[0].blob), function() {
+                                      writer.close(function(file) {
+                                        onSuccess();
+                                      });
+                                    });
+                                  });
+                              });
+                          }, function (error) {console.warn(error);});
+                      });
+                    }, function(currentIndex, totalIndex) {
+                      // onprogress callback
+                    }, function(error) {
+                      // onerror callback
+                  });
+              });
+          });
+
+	}
+    };
+
+    Whisper.ExportArchive = ExportArchive;
+})();
+
--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -333,70 +333,13 @@
         },
 
         exportMessages: function(e) {
-          var conversation_view = this;
-
-          this.window.chrome.fileSystem.chooseEntry( {
-              type: 'saveFile',
-              suggestedName: 'signal.zip',
-                accepts: [ { description: 'Zip files (*.zip)',
-                             extensions: ['zip']} ],
-                acceptsAllTypes: true
-              }, function (fileEntry) {
-                var shadow_view = new Whisper.ConversationStaticView({
-                    model: conversation_view.model,
-                    window: conversation_view.window
-                });
-
-                var export_doc = $('<html>')
-                        .append($('<head>')
-                            .append($('<link href="stylesheets/manifest.css" rel="stylesheet" type="text/css"/>'))
-                            .append($('<meta charset="utf-8"/>'))
-                            .append($('<meta content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" name="viewport"/>'))
-                            .append($('<meta name="viewport" content="width=device-width, initial-scale=1"/>'))
-                            .append($('<title>Signal</title>')))
-                        .append($('<body>').toggleClass('android')
-                            .append(shadow_view.el));
-
-                var conversation_source = export_doc[0].outerHTML;
-
-                var mediablobs = conversation_view.model.messageCollection
-                    .chain()
-                    .map(function (message) {
-                        return message.get('attachments').map(function (attachment,idx) {
-                            var url = 'media/'+ message.id + '-' + idx;
-                            return {
-                                url: url,
-                                blob: new Blob([attachment.data], {type: attachment.contentType})
-                           };
-                        });
-                    })
-                    .flatten()
-                    .value();
-
-                  zip.workerScriptsPath = '/components/zip.js/WebContent/';
-                  zip.createWriter(new zip.FileWriter(fileEntry,"application/zip"), function(writer) {
-                    writer.add("index.html", new zip.TextReader(conversation_source), function() {
-                      chrome.runtime.getPackageDirectoryEntry(function (packageDir) {
-                          packageDir.getFile('stylesheets/manifest.css',{},function (fileEntry) {
-                              fileEntry.file(function (blob) {
-                                  writer.add("stylesheets/manifest.css", new zip.BlobReader(blob), function() {
-                                    writer.add(mediablobs[0].url, new zip.BlobReader(mediablobs[0].blob), function() {
-                                      writer.close(function(file) {
-                                        // done
-                                        console.log('Export successful');
-                                      });
-                                    });
-                                  });
-                              });
-                          }, function (error) {console.warn(error);});
-                      });
-                    }, function(currentIndex, totalIndex) {
-                      // onprogress callback
-                    }, function(error) {
-                      // onerror callback
-                  });
-              });
-          });
+            var exporter = new Whisper.ExportArchive({
+                conversation: this.model,
+                window: this.window,
+            });
+            exporter.export(function (){
+                console.log('Export done');
+            });
         },
 
         sendMessage: function(e) {
